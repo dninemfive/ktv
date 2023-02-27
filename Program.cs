46e6454cf0f55d9880e6394edc2f58ebe2c37e8c
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-02-26 18:08:26 -0800

    slightly organize program.cs

--- Program.cs
+++ Program.cs
@@ -2,15 +2,18 @@
 using ktv;
 using System.IO;
 
-// ktv [interval] [times]
+# region constants
 const int MillisecondsPerMinute = 60 * 1000;
+string logPath = $"{DateTime.Now:yyyyMMddHHmmss}.ktv.log", aggregateLogPath = $"{DateTime.Now:yyyyMMddHHmmss}-aggregate.ktv.log";
+# endregion constants
+#region console args
 int delay = 5;
 float interval = 1;
 float duration = interval * 24 * 4;
 float aggregationInterval = 15;
-string logPath = $"{DateTime.Now:yyyyMMddHHmmss}.ktv.log", aggregateLogPath = $"{DateTime.Now:yyyyMMddHHmmss}-aggregate.ktv.log";
+#endregion console args
+# region local functions
 int ct = 0;
-
 void Log(object obj)
 {
     string str = obj.ToString() ?? obj.DefinitelyReadableString();
@@ -22,33 +25,33 @@ static void Sleep(int milliseconds)
 {
     Thread.Sleep(milliseconds);
 }
-foreach(string arg in args)
+#endregion local functions
+foreach (string arg in args)
 {
     ConsoleArg carg = new(arg);
     if (carg.Try<float?>(nameof(interval), s => float.TryParse(s, out float f) ? f : null) is float f) interval = f;
     if (carg.Try<float?>(nameof(duration), s => float.TryParse(s, out float f) ? f : null) is float g) duration = g;
     if (carg.Try<float?>(nameof(aggregationInterval), s => float.TryParse(s, out float f) ? f : null) is float h) aggregationInterval = h;
     if (carg.Try<int?>(nameof(delay), s => int.TryParse(s, out int i) ? i : null) is int i) delay = i;
-    if (carg.Try(nameof(logPath), s => s) is string s) logPath = s;
 }
 Console.WriteLine($"Beginning ktv. Will log active window title to {logPath} every {interval.Minutes()} for {duration.Minutes()} starting in {(delay / 60f).Minutes()}.");
 Console.WriteLine($"App usage will be aggregated and logged to {aggregateLogPath} every {aggregationInterval.Minutes()}.");
-float elapsed = 0;
 Sleep(delay * 1000);
-List<string> recentPrograms = new();
+List<string> aggregatedPrograms = new();
 float nextAggregationTime = aggregationInterval;
-while(duration < 0 || elapsed < duration)
+float elapsed = 0;
+while (duration < 0 || elapsed < duration)
 {
     ActiveWindowInfo info = ActiveWindow.Info;
-    recentPrograms.Add(info.Program);
+    aggregatedPrograms.Add(info.Program);
     Log(info);
     Sleep((int)(interval * MillisecondsPerMinute));
     elapsed += interval;
     if(elapsed >= nextAggregationTime)
     {
-        string mca = $"{DateTime.Now:HH:mm}\t{recentPrograms.MostCommon()}";
+        string mca = $"{DateTime.Now:HH:mm}\t{aggregatedPrograms.MostCommon()}";
         File.AppendAllText(aggregateLogPath, $"{mca}\n");
-        recentPrograms.Clear();
+        aggregatedPrograms.Clear();
         nextAggregationTime += aggregationInterval;
     }
 }
