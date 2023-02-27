f6fa2fc0a7390de2c43c37199f958311cc808832
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-02-26 18:16:44 -0800

    can now start at a different time

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ int delay = 5;
 float interval = 1;
 float duration = interval * 24 * 4;
 float aggregationInterval = 15;
+DateTime startAt = DateTime.Now + TimeSpan.FromMinutes(aggregationInterval);
 #endregion console args
 # region local functions
 int ct = 0;
@@ -33,12 +34,14 @@ foreach (string arg in args)
     if (carg.Try<float?>(nameof(duration), s => float.TryParse(s, out float f) ? f : null) is float g) duration = g;
     if (carg.Try<float?>(nameof(aggregationInterval), s => float.TryParse(s, out float f) ? f : null) is float h) aggregationInterval = h;
     if (carg.Try<int?>(nameof(delay), s => int.TryParse(s, out int i) ? i : null) is int i) delay = i;
+    if (carg.Try<DateTime?>(nameof(startAt), s => DateTime.TryParse(s, out DateTime dt) ? dt : null) is DateTime dt) startAt = dt;
 }
 Console.WriteLine($"Beginning ktv. Will log active window title to {logPath} every {interval.Minutes()} for {duration.Minutes()} starting in {(delay / 60f).Minutes()}.");
-Console.WriteLine($"App usage will be aggregated and logged to {aggregateLogPath} every {aggregationInterval.Minutes()}.");
+Console.WriteLine($"App usage will be aggregated and logged to {aggregateLogPath} every {aggregationInterval.Minutes()}, starting at {startAt:HH:mm}.");
 Sleep(delay * 1000);
 List<string> aggregatedPrograms = new();
-float nextAggregationTime = aggregationInterval;
+DateTime nextAggregationTime = startAt;
+TimeSpan aggregationTimespan = TimeSpan.FromMinutes(aggregationInterval);
 float elapsed = 0;
 while (duration < 0 || elapsed < duration)
 {
@@ -47,12 +50,12 @@ while (duration < 0 || elapsed < duration)
     Log(info);
     Sleep((int)(interval * MillisecondsPerMinute));
     elapsed += interval;
-    if(elapsed >= nextAggregationTime)
+    if(DateTime.Now > nextAggregationTime)
     {
         string mca = $"{DateTime.Now:HH:mm}\t{aggregatedPrograms.MostCommon()}";
         File.AppendAllText(aggregateLogPath, $"{mca}\n");
         aggregatedPrograms.Clear();
-        nextAggregationTime += aggregationInterval;
+        nextAggregationTime += aggregationTimespan;
     }
 }
 Console.WriteLine("Done.");
