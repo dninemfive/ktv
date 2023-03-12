ff266cca27b4f5a4ee3fc8a577e2e2bb73d959cc
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-03-12 15:47:02 -0700

    begin activityrecord thing

---
+++ ActivityRecord.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ktv
+{
+    internal class ActivityRecord
+    {
+        public DateTime StartedAt { get; private set; }
+        public DateTime EndedAt { get; private set; }
+        public string Activity { get; private set; }
+        public ActivityRecord(string activity)
+        {
+
+            EndedAt = DateTime.Now;
+            Activity = activity;
+        }
+    }
+}
---
+++ Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ktv
+{
+    public static class Constants
+    {
+        public const int MillisecondsPerMinute = 60 * 1000;
+        public static readonly string logPath = $"{DateTime.Now:yyyyMMddHHmmss}.ktv.log";
+    }
+}
