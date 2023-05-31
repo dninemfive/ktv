2b3326a5237a4c05717dbf8b02c869de841c5b02
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-05-31 00:48:42 -0700

    basic window name - activity distinction

---
+++ Activity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d9.ktv;
+internal class Activity
+{
+    string Name;
+    string Description;
+    string EventId;
+    DateTime Start;
+    DateTime End;
+    public Activity()
+    {
+
+    }
+}
