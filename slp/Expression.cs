0ca32bd05cf6f8a16b82db2ad69aa76ff9714245
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-04-15 12:15:10 -0700

    move files into folders; following the expression rabbit hole again

--- slp/Expression.cs
+++ slp/Expression.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using d9.slp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -8,13 +10,19 @@ using System.Threading.Tasks;
 
 namespace d9.ktv.slp;
 /*
- * Goal: allow writing conditions readably *and* parsably in JSON, e.g.
- * === Example 1 ===
- * {
- *      "time": {
- *          "or": "between
- *      }
- * }
- * === Example 2 ===
- * 
+ * Goal: flexibly and serializably express conditions in a convenient way
+ * e.g. for processes:  "(processName contains "minecraft") or (processFolder isIn "C:/Program Files (x86)/Steam")"
+ *      for scheduling: "((time after 12:30AM) and (time before 10:00AM)) or not(day is Sunday)"
  */
+public class ProcessExpression
+{
+    public Func<Process, string> Selector;
+    public Func<string, string, bool> Operand;
+    public string Value;
+    public bool Evaluate(Process process) => Operand(Selector(process), Value);
+}
+public class ProcessExpressionExpression
+{
+    public ProcessExpression[] Expressions;
+    public Func<bool[], bool> Operand;
+}
