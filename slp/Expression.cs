843ef13d9d623be653f343eca6c8d6ac48a7d2f3
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-04-15 11:57:17 -0700

    getting sidetracked again

---
+++ slp/Expression.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace d9.ktv.slp;
+/*
+ * Goal: allow writing conditions readably *and* parsably in JSON, e.g.
+ * === Example 1 ===
+ * {
+ *      "time": {
+ *          "or": "between
+ *      }
+ * }
+ * === Example 2 ===
+ * 
+ */
