987a0619955278e71537f7f486d4509f2f90db74
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-04-15 12:55:13 -0700

    maybe?

--- slp/Expression.cs
+++ slp/Expression.cs
@@ -1,4 +1,5 @@
 ﻿using d9.slp;
+using d9.utl;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,36 +15,39 @@ namespace d9.ktv;
  * e.g. for processes:  "(processName contains "minecraft") or (processFolder isIn "C:/Program Files (x86)/Steam")"
  *      for scheduling: "((time after 12:30AM) and (time before 10:00AM)) or not(day is Sunday)"
  */
-public abstract class Expression
+public class Assignment : Dictionary<string, object?> { }
+public delegate bool Expression(IReadOnlyDictionary<string, object?> assignment, params Expression[] children);
+public static class Expressions
 {
-    public abstract bool Evaluate(IReadOnlyDictionary<string, object?> assignment, params Expression[] children);
-}
-public class NotExpression : Expression
-{
-    public override bool Evaluate(IReadOnlyDictionary<string, object?> assignment, params Expression[] children)
+    public static bool Not(IReadOnlyDictionary<string, object?> assignment, params Expression[] children)
     {
         if (children.Length != 1)
             throw new ArgumentException($"Not expression must have exactly one child!", nameof(children));
-        return !children.First().Evaluate(assignment);
+        return !children.First()(assignment);
     }
-}
-public class OrExpression : Expression
-{
-    public override bool Evaluate(IReadOnlyDictionary<string, object?> assignment, params Expression[] children)
-        => children.EvaluateCollection(assignment, true, true);
-}
-public class AndExpression : Expression
-{
-    public override bool Evaluate(IReadOnlyDictionary<string, object?> assignment, params Expression[] children)
-        => children.EvaluateCollection(assignment, false, false);
-}
-public static class ExpressionExtensions
-{
-    public static bool EvaluateCollection(this IEnumerable<Expression> children, IReadOnlyDictionary<string, object?> assignment, bool successValue, bool anySucceeded)
+    private static bool CollectionExpressionBase(IReadOnlyDictionary<string, object?> assignment, IEnumerable<Expression> children, bool successValue, bool anySucceeded)
     {
         foreach (Expression child in children)
-            if (child.Evaluate(assignment) == successValue)
+            if (child(assignment) == successValue)
                 return anySucceeded;
         return !anySucceeded;
     }
+    public static bool Or(Assignment assignment, params Expression[] children)
+        => CollectionExpressionBase(assignment, children, true, true);
+    public static bool And(Assignment assignment, params Expression[] children)
+        => CollectionExpressionBase(assignment, children, false, false);
+    public static Expression Contains(string variableName, string expectedValue) 
+        => (assignment, children) 
+        => assignment[variableName] is string s && s.Contains(expectedValue);
+    public static Expression IsIn(string variableName, string expectedValue)
+        => (assignment, children)
+        => assignment[variableName] is string s && s.IsInFolder(expectedValue);
+    public static Assignment Assignment(this Process process) => new()
+    {
+        { "processName", process.ProcessName },
+        { "fileName", process.FileName() },
+        { "mainWindowTitle", process.MainWindowTitle }
+    };
+    public static bool ProcessExample(this Process process)
+        => Or(process.Assignment(), Contains("processName", "minecraft"), IsIn("fileName", "C:/Program Files (x86)/Steam"));
 }
