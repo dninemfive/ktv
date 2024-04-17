4fd480845f071763bcf17b6df2e65c2c882bcddc
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-04-17 11:05:31 -0700

    FixedPeriodTaskScheduler (not actually helpful, it turns out)

--- ktv/ActivityLogger/ActiveWindowLogger.cs
+++ ktv/ActivityLogger/ActiveWindowLogger.cs
@@ -1,18 +1,16 @@
-﻿using d9.utl;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Text.Json;
 
 namespace d9.ktv;
-public class ActiveWindowLogger(TimeSpan period) : TaskScheduler
+public class ActiveWindowLogger(TimeSpan period) : FixedPeriodTaskScheduler(period)
 {
-    public TimeSpan Period { get; private set; } = period;
     public int LogsSinceLastAggregation { get; private set; } = 0;
-    public override ScheduledTask NextTask(DateTime time)
+    protected override ScheduledTask NextTaskInternal(DateTime time)
     {
         string fileName = ActiveWindowLogUtils.FileNameFor(time);
-        return new(time.NextDayAlignedTime(Period), () => LogActiveWindow(fileName), this);
+        return new(time, () => LogActiveWindow(fileName), this);
     }
-    private void LogActiveWindow(string fileName)
+    private static void LogActiveWindow(string fileName)
     {
         Process? activeWindowProcess = ForegroundWindow.Process;
         ActiveWindowLogEntry entry = new(DateTime.Now, activeWindowProcess);
---
+++ ktv/TaskScheduler/FixedPeriodTaskScheduler.cs
@@ -0,0 +1,8 @@
+﻿namespace d9.ktv;
+public abstract class FixedPeriodTaskScheduler(TimeSpan period) : TaskScheduler
+{
+    public TimeSpan Period => period;
+    public override ScheduledTask NextTask(DateTime time)
+        => NextTaskInternal(time.NextDayAlignedTime(Period));
+    protected abstract ScheduledTask NextTaskInternal(DateTime time);
+}
