ab6ceca804fa2e95cb47daf734ad4474262be4fe
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-04-22 13:52:37 -0700

    why am i doing this AGAIN???

---
+++ ktv/Utils/expressions/SimpleExpression.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d9.ktv.Utils.expressions;
+[AttributeUsage(AttributeTargets.Method)]
+public class ExpressionOperatorAttribute(string name, Type inputType, Type outputType) : Attribute
+{
+    public string Name { get; private set; } = name;
+    public Type InputType { get; private set; } = inputType;
+    public Type OutputType { get; private set; } = outputType;
+}
+public class ExpressionTree
+{
+
+}
