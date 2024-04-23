3305a0d820e4ce3a9f4df5499b8172f8a0854d4f
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-04-23 09:33:15 -0700

    ??? (from yesterday)

--- ktv/Utils/expressions/SimpleExpression.cs
+++ ktv/Utils/expressions/SimpleExpression.cs
@@ -16,3 +16,45 @@ public class ExpressionTree
 {
 
 }
+// desired expressions:
+// Not(Expression<bool>) -> bool
+//      (NOT expr)
+// Or(Expression<bool>[]) -> bool
+//      (expr OR expr)
+// And(Expression<bool>[]) -> bool
+//      (expr AND expr)
+// IsInFolder(string, string) -> bool
+//      (expr INFOLDER expr)
+// RegexMatches(string, string) -> bool
+//      (expr MATCHES expr)
+// Get(string) -> object?
+//      (expr GET)
+public abstract class Expression
+{
+    public ExpressionResultMode Mode { get; set; }
+    public bool Apply(Assignment assignment);
+}
+public class ExpressionSequence
+{
+    public IEnumerable<Expression> Children { get; set; }
+    public bool Apply();
+}
+public enum ExpressionResultMode
+{
+    Standard,
+    Invert,     
+    Fail,
+    Succeed
+}
+public static class ExpressionExtensions
+{
+    public static bool ToResult(this bool input, ExpressionResultMode mode)
+        => mode switch
+        {
+            ExpressionResultMode.Standard => input,
+            ExpressionResultMode.Invert => !input,
+            ExpressionResultMode.Fail => false,
+            ExpressionResultMode.Succeed => true,
+            _ => throw new ArgumentOutOfRangeException(nameof(mode))
+        };
+}
