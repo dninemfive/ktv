fcfcfd0a41f86d8a4227417c4a29425b07ba18a1
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-04-28 10:05:56 -0700

    fully test Extensions

--- ktv.tests/ktv.tests/Tests_ActiveWindowLogEntry.cs
+++ ktv.tests/ktv.tests/Tests_ActiveWindowLogEntry.cs
@@ -1,11 +1,4 @@
-﻿using d9.ktv;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ktv.tests;
+﻿namespace d9.ktv.tests;
 [TestClass]
 public class Tests_ActiveWindowLogEntry
 {
---
+++ ktv.tests/ktv.tests/Tests_Extensions.cs
@@ -0,0 +1,47 @@
+﻿using d9.utl.compat;
+
+namespace d9.ktv.tests;
+[TestClass]
+public class Tests_Extensions
+{
+    [TestMethod]
+    public void Test_IsInt()
+    {
+        Assert.IsTrue(2.0.IsInt());
+    }
+    [TestMethod]
+    public void Test_IsInt_IsNotInt()
+    {
+        Assert.IsFalse(2.5.IsInt());
+    }
+    [TestMethod]
+    public void Test_ToColorId()
+    {
+        foreach(int i in Enum.GetValuesAsUnderlyingType<GoogleUtils.EventColor>())
+            Assert.AreEqual(i.ToString(), ((GoogleUtils.EventColor)i).ToColorId());
+    }
+    [TestMethod]
+    public void Test_toCamelCase_0LengthString()
+    {
+        Assert.AreEqual("", "".toCamelCase());
+    }
+    [TestMethod]
+    public void Test_toCamelCase_1LengthString()
+    {
+        Assert.AreEqual("a", "a".toCamelCase());
+        Assert.AreEqual("a", "A".toCamelCase());
+    }
+    [TestMethod]
+    public void Test_toCamelCase()
+    {
+        Assert.AreEqual("anExampleString", "AnExampleString".toCamelCase());
+        Assert.AreEqual("anexamplestring", "anexamplestring".toCamelCase());
+        Assert.AreEqual("anExampleString", "anExampleString".toCamelCase());
+    }
+    [TestMethod]
+    public void Test_MultilineListWithAlignedTitle()
+    {
+        Assert.AreEqual("list:   test\n" +
+                        "        item2", Extensions.MultilineListWithAlignedTitle(["test", "item2"], "list:"));
+    }
+}
