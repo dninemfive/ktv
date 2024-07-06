297cf3d0e85ecf7f2038d2f1bd07e7d7fbf3a1b1
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-07-06 12:52:02 -0600

    stuff

---
+++ ktv/KtvServiceInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d9.ktv;
+public class KtvServiceInfo
+{
+    public required string Output;
+    public required TimeSpan Uptime;
+
+}
+public class ProcessCloserInfo
+{
+
+}
