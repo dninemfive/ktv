921312e4682fbf1dd9418cdc52b3cfac3baf7d85
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-07-14 01:38:06 -0600

    make logic much more straightforward, if a bit more verbose

--- ktv/activities/aggregation/GoogleCalendarEventManager.cs
+++ ktv/activities/aggregation/GoogleCalendarEventManager.cs
@@ -1,12 +1,12 @@
 ﻿using d9.utl.compat;
 using Google.Apis.Calendar.v3.Data;
+using EventStartTime = (System.DateTime startTime, string eventId);
 
 namespace d9.ktv;
 public class GoogleCalendarEventManager
 {
     public ActivityAggregationConfig Config { get; private set; }
-    private readonly Dictionary<Activity, DateTime> _startTimes = new();
-    private readonly Dictionary<Activity, string?> _eventIds = new();
+    private readonly Dictionary<Activity, EventStartTime> _startedEvents = new();
     private GoogleCalendarEventManager(ActivityAggregationConfig cfg)
         => Config = cfg;
     public static GoogleCalendarEventManager? From(ActivityAggregationConfig config)
@@ -19,53 +19,47 @@ public class GoogleCalendarEventManager
     {
         foreach (Activity activity in summary)
         {
-            if (!_startTimes.TryGetValue(activity, out DateTime start))
+            if(_startedEvents.TryGetValue(activity, out EventStartTime eventStartTime))
             {
-                start = summary.Start;
-                _startTimes[activity] = start;
+                if (summary[activity] >= Config.GoogleCalendar?.ActivityPercentageThreshold)
+                {
+                    (DateTime start, string id) = eventStartTime;
+                    UpdateEvent(id, activity, start, summary.End);
+                } 
+                else
+                {
+                    _startedEvents.Remove(activity);
+                }
             }
-            if (summary[activity] < Config.GoogleCalendar?.ActivityPercentageThreshold)
+            else if (summary[activity] >= Config.GoogleCalendar?.ActivityPercentageThreshold)
             {
-                Remove(activity);
-                continue;
-            }
-            _eventIds[activity] = TryPostEvent(activity, start, summary.End);
-        }
-    }
-    private void Remove(Activity activity)
-    {
-        void printDicts(string msg)
-        {
-            Console.WriteLine(msg);
-            foreach (Activity key in _startTimes.Keys.Union(_eventIds.Keys)
-                                                     .Distinct()
-                                                     .OrderBy(x => x.Category)
-                                                     .ThenBy(x => x.Name))
-            {
-                Console.WriteLine($"{key,-20}\t{(_startTimes.TryGetValue(key, out DateTime value) ? value : ""),-20}" +
-                                  $"\t{(_eventIds.TryGetValue(key, out string? id) ? id : "")}");
+                string? id = TryPostEvent(activity, summary.Start, summary.End);
+                if (id is string actualId)
+                    _startedEvents.Add(activity, (summary.Start, actualId));
             }
         }
-        printDicts("before:");
-        Console.WriteLine($"Remove {activity}");
-        _startTimes.Remove(activity);
-        _eventIds.Remove(activity);
-        printDicts("after:");
     }
-    public string? TryPostEvent(Activity activity, DateTime start, DateTime end)
+    public void UpdateEvent(string eventId, Activity activity, DateTime start, DateTime end)
     {
-        _eventIds.TryGetValue(activity, out string? existingId);
-        Event @event = activity.ToEvent(start, end, Config.ColorFor(activity.Category));
-        string calendarId = Config.GoogleCalendar!.Id;
         try
         {
-            return existingId is null ? GoogleUtils.AddEventTo(calendarId, @event).Id
-                                      : GoogleUtils.UpdateEvent(calendarId, existingId, @event).Id;
-        } 
+            _ = GoogleUtils.UpdateEvent(Config.GoogleCalendar!.Id, eventId, activity.ToEvent(start, end, Config.ColorFor(activity.Category)));
+        }
         catch(Exception e)
         {
             Console.Error.WriteLine($"{e.GetType().Name}: {e.Message}");
         }
-        return null;
+    }
+    public string? TryPostEvent(Activity activity, DateTime start, DateTime end)
+    {
+        try
+        {
+            return GoogleUtils.AddEventTo(Config.GoogleCalendar!.Id, activity.ToEvent(start, end, Config.ColorFor(activity.Category))).Id;
+        }
+        catch (Exception e)
+        {
+            Console.Error.WriteLine($"{e.GetType().Name}: {e.Message}");
+            return null;
+        }
     }
 }
