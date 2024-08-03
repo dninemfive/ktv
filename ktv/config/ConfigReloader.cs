67dcc0121c2583fb95e6932a7f59b0889bf78f69
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-08-03 11:02:35 -0600

    start work on config reloader (#5)

---
+++ ktv/config/ConfigReloader.cs
@@ -0,0 +1,12 @@
+﻿using d9.utl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d9.ktv;
+public class ConfigReloader(KtvConfig config, Log log)
+    : FixedPeriodTaskScheduler(TimeSpan.FromMinutes(config.ReloadPeriod), log)
+{
+}
