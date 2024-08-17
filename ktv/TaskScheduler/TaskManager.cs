22b1db0e5303933334afe1d3b99d77069b43d953
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-08-17 11:24:56 -0600

    i kept forgetting to actually add TaskManager lol

---
+++ ktv/TaskScheduler/TaskManager.cs
@@ -0,0 +1,37 @@
+﻿using d9.ktv.ActivityLogger;
+using d9.utl;
+using System.Collections;
+
+namespace d9.ktv;
+public class TaskManager(IEnumerable<TaskScheduler> schedulers)
+    : IEnumerable<TaskScheduler>
+{
+    private readonly List<TaskScheduler> _schedulers = [.. schedulers];
+    public IEnumerator<TaskScheduler> GetEnumerator()
+        => ((IEnumerable<TaskScheduler>)_schedulers).GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator()
+        => ((IEnumerable)_schedulers).GetEnumerator();
+    private bool _running = false;
+    private readonly List<Task<TaskScheduler>> _scheduledTasks = [];
+    public IEnumerable<Task<TaskScheduler>> ScheduledTasks => _scheduledTasks;
+    public async Task Run()
+    {
+        _running = !_running ? true : throw new Exception("Attempted to run a TaskManager which was already running!");
+        DateTime now = DateTime.Now;
+        foreach (TaskScheduler scheduler in _schedulers)
+        {
+            scheduler.SetUp();
+            _scheduledTasks.Add(scheduler.NextTask(now));
+        }
+        while (_scheduledTasks.Any())
+        {
+            Task<TaskScheduler> nextCompletedTask = await Task.WhenAny(_scheduledTasks);
+            _scheduledTasks.Remove(nextCompletedTask);
+            TaskScheduler scheduler = await nextCompletedTask;
+            _scheduledTasks.Add(scheduler.NextTask(DateTime.Now));
+        }
+    }
+    public T? First<T>()
+        where T : TaskScheduler
+        => _schedulers.Any() ? _schedulers.OfType<T>().First() : null;
+}
