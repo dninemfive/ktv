d070a0a0399d81176bc74105c6fb310de3f03020
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-08-17 11:38:16 -0600

    fix compilation errors in ktv.gui

--- ktv.gui/MainWindow.xaml.cs
+++ ktv.gui/MainWindow.xaml.cs
@@ -18,27 +18,25 @@ namespace d9.ktv.gui;
 public partial class MainWindow : Window
 {
     public Log Log { get; private set; }
-    private readonly Progress<string> _serviceLog;
-    public KtvService Service { get; private set; }
+    public KtvService? Service { get; private set; }
     public MainWindow()
     {
         InitializeComponent();
-        Log = new(DateTime.Now.GenerateLogFile(), Console, mode: Log.Mode.WriteImmediate);
-        _serviceLog = new(Log.WriteLine);
-        string configPath = "config.json";
-        KtvConfig? config = Config.TryLoad<KtvConfig>(configPath);
-        if (config is null)
-        {
-            Log.WriteLine($"Could not find config at {configPath.AbsolutePath()}! Using default config...");
-            config = KtvConfig.Default;
-        }
-        Service = KtvService.CreateAndLog(config, _serviceLog);
+        Log = new(Log.Components.Console, Log.Components.WriteTextTo(DateTime.Now.GenerateLogFile()));
     }
 
     private async void StartButton_Click(object sender, RoutedEventArgs _)
     {
         if (sender is Button b)
             b.Visibility = Visibility.Hidden;
+        string configPath = "config.json";
+        KtvConfig? config = Config.TryLoad<KtvConfig>(configPath, out string? _);
+        if (config is null)
+        {
+            await Log.WriteLine($"Could not find config at {configPath.AbsoluteOrInBaseFolder()}! Using default config...");
+            config = KtvConfig.Default;
+        }
+        Service = await KtvService.CreateAndLog(config, Log);
         Task runningService = Service.Run();
         foreach (TaskScheduler scheduler in Service.Schedulers)
             ProgressBars.Add(scheduler);
